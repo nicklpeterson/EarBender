06df3921d0e6eede7256ddccddfaa20dc8d784c7
arxii-13 <wzq950720@hotmail.com>
2020-09-28 10:48:26 -0700

    Setup a maven project

---
+++ MusicDSL/src/main/java/ui/Main.java
@@ -0,0 +1,23 @@
+package ui;
+
+
+import org.jfugue.pattern.Pattern;
+import org.jfugue.player.Player;
+import org.jfugue.theory.ChordProgression;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pattern pattern = new ChordProgression("I IV V")
+                .distribute("7%6")
+                .allChordsAs("$0 $0 $0 $0 $1 $1 $0 $0 $2 $1 $0 $0")
+                .eachChordAs("$0ia100 $1ia80 $2ia80 $3ia80 $4ia100 $3ia80 $2ia80 $1ia80")
+                .getPattern()
+                .setInstrument("Piano")
+                .setTempo(100);
+        new Player().play(pattern);
+
+    }
+
+}
