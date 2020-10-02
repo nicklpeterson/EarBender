23ca1c3420f6d0488cc63c547a19ee6c5bd105b5
Nick Peterson <nick.peterson@alumni.ubc.ca>
2020-10-02 14:35:33 -0700

    Add jfugue remote repo, slf4j logging dependency, and lombok

--- src/main/java/com/dslproject/ui/Main.java
+++ src/main/java/com/dslproject/ui/Main.java
@@ -1,6 +1,8 @@
-package ui;
+package com.dslproject.ui;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
 import org.jfugue.theory.ChordProgression;
@@ -8,6 +10,8 @@ import org.jfugue.theory.ChordProgression;
 public class Main {
 
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Starting Main");
 
         Pattern pattern = new ChordProgression("I IV V")
                 .distribute("7%6")
@@ -16,7 +20,7 @@ public class Main {
                 .getPattern()
                 .setInstrument("Piano")
                 .setTempo(100);
-        new Player().play(pattern);
+        new Player().play(pattern.repeat(1));
 
     }
 
