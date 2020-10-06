ed2e0381212a130b867911c5bdb7e3bf9b11de31
Nick Peterson <nick.peterson@alumni.ubc.ca>
2020-10-05 21:37:07 -0700

    Add remaining fixed literals

--- src/main/java/com/dslproject/util/DslConstants.java
+++ src/main/java/com/dslproject/util/DslConstants.java
@@ -6,11 +6,27 @@ import java.util.List;
 public class DslConstants {
     final static public String SET_REGEX = "SET [A-Za-z0-9]+";
     final static public String NOTES_REGEX = "NOTES\\(([A-G](\\[(q|h|w)*\\])?)(,( *)([A-G](\\[(q|h|w)*\\])?))*\\)";
-    // TODO: Add more instruments
-    final static public String INSTRUMENT_REGEX = "INSTRUMENT\\(((piano)|(violin)|(trumpet)|(guitar)|(bass))\\)";
+    final static public String INSTRUMENT_REGEX = "INSTRUMENT\\(((piano)|(violin)|(trumpet)|(guitar)|(bass))\\)"; // TODO: Add more instruments
     final static public String TEMPO_REGEX = "TEMPO\\(([0-9]+)\\)";
     final static public String RHYTHM_REGEX = "RHYTHM LAYER1\\((H|B|S|C|.){8}\\) LAYER2\\((H|B|S|C|.){8}\\) LAYER3\\((H|B|S|C|.){8}\\)";
+    final static public String PLAY_REGEX = "PLAY";
+    final static public String LOOP_REGEX = "LOOP";
+    final static public String START_REGEX = "START [A-Za-z0-9]+(,[A-Za-z0-9]+)*";
+    final static public String END_REGEX = "END";
+    final static public String NUMBER_REGEX = "[0-9]+";
+    final static public String STOP_REGEX = "STOP";
 
     // Make sure to add new regex strings here so they get passed to the tokenizer
-    final static public List<String> REGEX_LIST = Arrays.asList(SET_REGEX, NOTES_REGEX, INSTRUMENT_REGEX, TEMPO_REGEX, RHYTHM_REGEX);
+    final static public List<String> REGEX_LIST = Arrays.asList(
+            SET_REGEX,
+            NOTES_REGEX,
+            INSTRUMENT_REGEX,
+            TEMPO_REGEX,
+            RHYTHM_REGEX,
+            PLAY_REGEX,
+            LOOP_REGEX,
+            START_REGEX,
+            END_REGEX,
+            NUMBER_REGEX,
+            STOP_REGEX);
 }
